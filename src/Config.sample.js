cfd9a96c6de1fb493bc59d76b7a74029f26c2965
jatinder.singh <jatinder.singh@netsolutions.com>
2021-05-05 18:39:19 +0530

    add config sample

---
+++ src/Config.sample.js
@@ -0,0 +1,9 @@
+export const config = {
+    appId: '[APP-ID]',
+    redirectUri:'http://localhost:3000',
+    scopes:['user.read'],
+    authority:'https://login.microsoftonline.com/[TENANT-ID-HERE]',
+};
+
+// https://docs.microsoft.com/en-us/azure/active-directory/develop/msal-client-application-configuration#authority
+
